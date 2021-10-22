443eb217f43dd017938219d76c4d7df25bd0a46a
Sergio Pachon Dotor <sap9827@gmail.com>
2021-10-22 14:55:39 -0500

    PresidencialesApp V 1.0

---
+++ src/main/java/com/mugpork/Presidenciales/PresidencialesApplication.java
@@ -0,0 +1,13 @@
+package com.mugpork.Presidenciales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresidencialesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresidencialesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mugpork/Presidenciales/PresidencialesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mugpork.Presidenciales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresidencialesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
